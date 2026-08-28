6b73507a3f9d87c655c433ec69de939a5931f649
tinhosys <tinhopvh@gmail.com>
2026-08-27 23:33:05 -0400

    style: remove process number from name column

---
+++ patch_remove_processos.cjs
@@ -0,0 +1,10 @@
+﻿const fs = require('fs');
+const path = require('path');
+const file = path.join(__dirname, 'js', 'diarias.js');
+let content = fs.readFileSync(file, 'utf8');
+
+const regex = /<br><span style="color:#64748b; font-weight:normal; font-size:10px;">Proc: \\\$\\{d\.processo\\}<\/span>/g;
+content = content.replace(regex, '');
+
+fs.writeFileSync(file, content);
+console.log('Removed processos from name column');
